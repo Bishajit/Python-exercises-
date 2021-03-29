2a90105eb390aee90063f66db0ec48068ab6fedc
Bishajit <67888547+Bishajit@users.noreply.github.com>
2021-03-28 20:37:49 -0400

    Update LeetCode_ThreeConsecutiveOdds.py

--- LeetCode_ThreeConsecutiveOdds.py
+++ LeetCode_ThreeConsecutiveOdds.py
@@ -1,6 +1,6 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        for n in range(len(arr)):
+        for n in range(len(arr)-2):
             if arr[n] % 2 == 1 and arr[n+1] % 2 == 1 and arr[n+2] % 2 == 1:
                 return True
         return False
