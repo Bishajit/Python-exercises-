b457dc98eca1992110d5285b073c3837cfe0185a
Bishajit <67888547+Bishajit@users.noreply.github.com>
2021-03-28 20:38:44 -0400

    Update LeetCode_ThreeConsecutiveOdds.py

--- LeetCode_ThreeConsecutiveOdds.py
+++ LeetCode_ThreeConsecutiveOdds.py
@@ -4,3 +4,4 @@ class Solution:
             if arr[n] % 2 == 1 and arr[n+1] % 2 == 1 and arr[n+2] % 2 == 1:
                 return True
         return False
+    #https://leetcode.com/problems/three-consecutive-odds/
